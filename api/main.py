8d316d8ed79c04cc3cdee0338acd6508f5d581c6
beiyanpiki <i@mki.moe>
2023-01-28 18:44:38 +0800

    add cors support

--- api/main.py
+++ api/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.routers import bili
 from api.utils import response as resp
 
 app = FastAPI()
 
+# CORS Support
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://mki.moe", "https://mik.moe"],
+    allow_origin_regex=['https://.*\.mki\.moe', "https://.*\.mik\.moe"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Bilibili Api
 app.include_router(bili.router)
 
 
